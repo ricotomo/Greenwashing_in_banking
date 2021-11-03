ec4cadf7157b832e49548ca17f15175e948f4c9f
Moritz Walther <friedicecream1@gmail.com>
2021-11-03 16:05:52 +0100

    update

--- main.py
+++ main.py
@@ -19,16 +19,51 @@ from nltk.stem import SnowballStemmer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from sklearn.feature_extraction.text import CountVectorizer
 
-# Twitter authentication
+# set creds
 result = pd.read_csv('C:/Users/morit/Documents/School/MSc - Research Project (Semester 3)/creds.csv')
 print(result)
-API_key=result.loc[0]['value']
-API_Key_Secret=result.loc[1]['value']
+CONSUMER_KEY=result.loc[0]['value']
+CONSUMER_SECRET=result.loc[1]['value']
 Bearer_Token=result.loc[2]['value']
+ACCESS_TOKEN=result.loc[3]['value']
+ACCESS_TOKEN_SECRET=result.loc[4]['value']
+print(CONSUMER_KEY)
+print(CONSUMER_SECRET)
+print(ACCESS_TOKEN)
+print(ACCESS_TOKEN_SECRET)
 
-# consumerKey = “Type your consumer key here”
-# consumerSecret = “Type your consumer secret here”
-# accessToken = “Type your accedd token here”
-# accessTokenSecret = “Type your access token secret here”auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-# auth.set_access_token(accessToken, accessTokenSecret)
-# api = tweepy.API(auth)
+
+# Utility function to clean tweet text by removing links, special characters
+# using simple regex statements.
+def clean_tweet(self, tweet):       
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+# Authenticate to Twitter
+try:
+    # create OAuthHandler object
+    auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+    # set access token and secret
+    auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+    # Create API object
+    api = tweepy.API(auth)
+    print(api.verify_credentials().name)
+except:
+    print("Error: Authentication Failed")
+    
+
+#Get data
+keyword = "lockdown2london"
+noOfTweets = int(5)
+tweets = tweepy.Cursor(api.search_tweets(q=keyword, count=noOfTweets))
+
+for tweet in tweets:
+ print(tweet.text)
+
+
+ # creating object of TwitterClient Class
+    # api = TwitterClient()
+    # # calling function to get tweets
+    # tweets = api.get_tweets(query = 'Donald Trump', count = 200)
+
+# call twitter api to fetch tweets
+# fetched_tweets = self.api.search(q = query, count = count)
